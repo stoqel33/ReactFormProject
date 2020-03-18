160bb38a246311d7561e1971d49d0241543eebd8
Konrad <konrad.stochel.13@gmail.com>
2020-03-18 12:02:54 +0100

    form validation, display errors

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,54 +1,62 @@
-import React from 'react'
+import React from 'react';
+import './Form.css';
 
-const Form = ({ name, password, email, accept, change, submit }) => (
-  <form onSubmit={submit} noValidate>
-    <label htmlFor="name">
-      Name:
+const Form = ({ name, password, email, accept, send, change, submit, errors }) => (
+  <>
+    <form onSubmit={submit} noValidate>
+      <label htmlFor="name">
+        Name:
       <input
-        type="text"
-        id="name"
-        name="userName"
-        value={name}
-        onChange={change}
-      />
+          type="text"
+          id="name"
+          name="userName"
+          value={name}
+          onChange={change}
+        />
+        {errors.name && <span> bład </span>}
+      </label>
+
+      <label htmlFor="password">
+        Password:
+      <input
+          type="password"
+          id="password"
+          name="userPassword"
+          value={password}
+          onChange={change}
+        />
+        {errors.pass && <span> błąd </span>}
+      </label>
+
+      <label htmlFor="email">
+        Email:
+      <input
+          type="email"
+          id="emial"
+          name="userEmail"
+          value={email}
+          onChange={change}
+        />
+        {errors.email && <span> błąd </span>}
+      </label>
+
+      <label htmlFor="accept">
+        <input
+          type="checkbox"
+          name="userAccept"
+          id="accept"
+          checked={accept}
+          onChange={change}
+        />
+        {errors.accept && <span> błąd </span>}
+      I accept all rules!
     </label>
 
-    <label htmlFor="password">
-      Password:
-      <input
-        type="password"
-        id="password"
-        name="userPassword"
-        value={password}
-        onChange={change}
-      />
-    </label>
+      <button>Join in!</button>
 
-    <label htmlFor="email">
-      Email:
-      <input
-        type="email"
-        id="emial"
-        name="userEmail"
-        value={email}
-        onChange={change}
-      />
-    </label>
-
-    <label htmlFor="accept">
-      <input
-        type="checkbox"
-        name="userAccept"
-        id="accept"
-        checked={accept}
-        onChange={change}
-      />
-      I agree all rules!
-    </label>
-
-    <button>Join in!</button>
-
-  </form>
+    </form>
+    {send && <h1>Wysłano</h1>}
+  </>
 )
 
 export default Form;
