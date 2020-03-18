651b2bcb17fd6ae7ca4faca2cd91242ed1705836
Konrad <konrad.stochel.13@gmail.com>
2020-03-18 12:01:59 +0100

    add errors and form validation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,11 +8,86 @@ class App extends React.Component {
     userPassword: '',
     userEmail: '',
     userAccept: false,
+    send: false,
+
+    errors: {
+      name: false,
+      pass: false,
+      email: false,
+      accept: false,
+    },
   }
 
   handleSubmitForm = (e) => {
     e.preventDefault();
-    console.log('dziala');
+
+    const validationCheck = this.validationForm();
+    console.log(validationCheck);
+
+
+    if (validationCheck.validation) {
+      console.log('dziala');
+
+      this.setState({
+        userName: '',
+        userPassword: '',
+        userEmail: '',
+        userAccept: false,
+        send: true,
+
+        errors: {
+          name: false,
+          pass: false,
+          email: false,
+          accept: false,
+        },
+      })
+      console.log('wyslano');
+
+    } else {
+      this.setState({
+        errors: {
+          name: !validationCheck.name,
+          pass: !validationCheck.pass,
+          email: !validationCheck.email,
+          accept: !validationCheck.accept,
+        },
+      })
+    }
+  }
+
+  validationForm = () => {
+    let name = false;
+    let pass = false;
+    let email = false;
+    let accept = false;
+    let validation = false;
+
+    if (this.state.userName.length > 6 &&
+      this.state.userName.indexOf(' ') === -1) {
+      name = true;
+    };
+    if (this.state.userEmail.indexOf('@') != -1 &&
+      this.state.userEmail.length > 6) {
+      email = true;
+    };
+    if (this.state.userPassword.length > 6) {
+      pass = true;
+    };
+    if (this.state.userAccept) {
+      accept = true;
+    };
+    if (name && pass && email && accept) {
+      validation = true;
+    };
+
+    return ({
+      name,
+      pass,
+      email,
+      accept,
+      validation
+    })
 
   }
 
@@ -26,7 +101,14 @@ class App extends React.Component {
         [e.target.name]: e.target.value,
       })
     }
+  }
 
+  componentDidUpdate() {
+    if (this.state.send === true) {
+      setTimeout(() => this.setState({
+        send: false
+      }), 3000)
+    }
   }
 
   render() {
@@ -36,8 +118,10 @@ class App extends React.Component {
         password={this.state.userPassword}
         email={this.state.userEmail}
         accept={this.state.userAccept}
+        send={this.state.send}
         change={this.handleInputChange}
         submit={this.handleSubmitForm}
+        errors={this.state.errors}
       />
     );
   }
